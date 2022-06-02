8fe6dd43f4c1ac5acea9690b3f588c99a84b3c7a
wingxia <wingxiaa@gmail.com>
2022-06-02 10:53:30 +0800

    ws://

--- main-saya.py
+++ main-saya.py
@@ -13,7 +13,7 @@ from graia.scheduler.saya import GraiaSchedulerBehaviour
 app = Ariadne(
     MiraiSession(
         # 以下3行请按照你的 MAH 配置来填写
-        host="http://10.144.17.0:8080",  # 同 MAH 的 port
+        host="ws://10.144.17.0:8080",  # 同 MAH 的 port
         verify_key="wingxiaismydad",  # 同 MAH 配置的 verifyKey
         account=3965749,  # 机器人 QQ 账号
     ),
