67560c5a6ea90b5cc933ae4f89fc76b71e721740
wingxia <wingxiaa@gmail.com>
2022-06-06 14:47:38 +0800

    莫名其妙的禁言时间

--- modules/forbidden_words.py
+++ modules/forbidden_words.py
@@ -2,11 +2,12 @@ from function.rsql import run_sql
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.message.element import At, Image, Plain,Source
+from graia.ariadne.message.element import At, Image, Plain, Source
 from graia.ariadne.model import Group, Member
 from graia.saya import Channel
 from graia.saya.builtins.broadcast import ListenerSchema
 import pypinyin
+
 channel = Channel.current()
 
 forbiddingWords = run_sql(f"select words from fb_words")
@@ -17,15 +18,14 @@ forbiddingWords = run_sql(f"select words from fb_words")
         listening_events=[GroupMessage],
     )
 )
-async def forbidden_words(app: Ariadne,source:Source, group: Group, message: MessageChain, member: Member):
+async def forbidden_words(app: Ariadne, source: Source, group: Group, message: MessageChain, member: Member):
     for forbiddenWord in forbiddingWords:
         if forbiddenWord in message.asDisplay():
             fb_c = run_sql(f"select * from fb_words where words = '{forbiddenWord}'")
-            print(fb_c)
             if fb_c[2] or group.id in fb_c[2]:
                 await app.recallMessage(source)
+                await app.muteMember(group, member, int(fb_c[1]))
                 print(fb_c[1])
-                await app.muteMember(group, member, fb_c[1])
                 fb_pinyin = pypinyin.pinyin(f"{forbiddenWord}")
                 await app.sendGroupMessage(group, MessageChain.create(
                     At(member), Plain(f"！欢迎进入小黑屋，以后可不要来了。\n触发违禁词'{fb_pinyin}'")
