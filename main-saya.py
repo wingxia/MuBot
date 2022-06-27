c05f6ad1615537b868e052dedbdcbe87578b77e8
wingxia <wingxiaa@gmail.com>
2022-06-27 10:30:56 +0800

    主进程导入优化

--- main-saya.py
+++ main-saya.py
@@ -1,28 +1,32 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
+import asyncio
 import pkgutil
 
-from graia.ariadne.app import Ariadne
-from graia.ariadne.model import MiraiSession
 from graia.saya import Saya
+from graia.ariadne import Ariadne
+from graia.ariadne.connection.config import (
+    HttpClientConfig,
+    WebsocketClientConfig,
+    config,
+)
+from graia.broadcast import Broadcast
 from graia.saya.builtins.broadcast import BroadcastBehaviour
-from graia.scheduler import GraiaScheduler
-from graia.scheduler.saya import GraiaSchedulerBehaviour
 
+loop = asyncio.new_event_loop()
+bcc = Broadcast(loop=loop)
+Ariadne.config(loop=loop, broadcast=bcc)
 app = Ariadne(
-    MiraiSession(
-        # 以下3行请按照你的 MAH 配置来填写
-        host="http://10.144.17.0:8080",  # 同 MAH 的 port
-        verify_key="wingxiaismydad",  # 同 MAH 配置的 verifyKey
-        account=1691641,  # 机器人 QQ 账号
+    connection=config(
+        1691641,  # 你的机器人的 qq 号
+        "wingxiaismydad",  # 填入 verifyKey
+        # 以下两行是你的 mirai-api-http 地址中的地址与端口
+        # 默认为 "http://localhost:8080" 如果你没有改动可以省略这两行
+        HttpClientConfig(host="http://10.144.17.0:8080"),
+        WebsocketClientConfig(host="http://10.144.17.0:8080"),
     ),
 )
-app.create(GraiaScheduler)
 saya = app.create(Saya)
 saya.install_behaviours(
     app.create(BroadcastBehaviour),
-    app.create(GraiaSchedulerBehaviour),
 )
 
 with saya.module_context():
