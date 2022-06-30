144f729eabc7260680a8a332e99213a417f8f995
wingxia <wingxiaa@gmail.com>
2022-06-30 12:10:52 +0800

    添加了获取图片id功能

--- function/small_tool.py
+++ function/small_tool.py
@@ -1,3 +1,7 @@
-def get_img_id(pr_str):
+from graia.ariadne.message.chain import MessageChain
+
+
+def get_img_id(message: MessageChain):
+    pr_str = message.as_persistent_string()
     id_index = pr_str.find('imageId') + 10
     return pr_str[id_index:id_index + 42]
--- modules/temp.py
+++ modules/temp.py
@@ -1,9 +1,9 @@
 from graia.ariadne.app import Ariadne
-from graia.ariadne.event.message import GroupMessage,FriendMessage
+from graia.ariadne.event.message import FriendMessage
 from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.message.element import At, Image, Plain
-from graia.ariadne.model import Group, Member, MemberPerm
-from graia.ariadne.message.parser.twilight import FullMatch, Twilight, RegexMatch, ElementMatch
+from graia.ariadne.message.element import Image, Plain
+from graia.ariadne.message.parser.base import MatchTemplate
+from graia.ariadne.model import Friend
 from graia.saya import Channel
 from graia.saya.builtins.broadcast import ListenerSchema
 from function.small_tool import get_img_id
@@ -14,11 +14,9 @@ channel = Channel.current()
 @channel.use(
     ListenerSchema(
         listening_events=[FriendMessage],
-        # inline_dispatchers=[Twilight(ElementMatch(Image))]
+        decorators=[MatchTemplate([Plain("id"), Image])],
     )
 )
-async def send_img_id(app: Ariadne,message: MessageChain):
-    message.asPersistentString()
-    # print(message.asPersistentString())
-    # img_id = get_img_id(pr_message)
-    # await app.sendGroupMessage(group, MessageChain.create(Plain(img_id)))
+async def send_img_id(app: Ariadne, message: MessageChain, friend: Friend):
+    img_id = get_img_id(message)
+    await app.send_friend_message(friend, MessageChain.create(Plain(img_id)))
